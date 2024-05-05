32a9b3591392804fc6de336320661a5c1472788b
ZSFan <123727427+Zsfan1224@users.noreply.github.com>
2024-05-06 07:59:41 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -725,7 +725,7 @@ ${วเลสSec}
 	const clash_link = `https://api.v1.mk/sub?target=clash&url=${encodeURIComponent(sublink)}&insert=false&emoji=true&list=false&tfo=false&scv=true&fdn=false&sort=false&new_name=true`;
 	// Prepare header string
 	const header = `
-<p align='center'><img src='https://cloudflare-ipfs.com/ipfs/bafybeigd6i5aavwpr6wvnwuyayklq3omonggta4x2q7kpmgafj357nkcky' alt='图片描述' style='margin-bottom: -50px;'>
+<p align='center'><img src='https://telegram-5yk.pages.dev/file/7a9ccd2554c182d659c13.jpg' alt='图片描述' style='margin-bottom: -50px;'>
 <b style='font-size: 15px;'>Welcome! This function generates configuration for วเลส protocol. If you found this useful, please check our GitHub project for more:</b>
 <b style='font-size: 15px;'>欢迎！这是生成 วเลส 协议的配置。如果您发现这个项目很好用，请查看我们的 GitHub 项目给我一个star：</b>
 <a href='https://github.com/Zsfan1224/Cloudflare-Pages-Proxy' target='_blank'>Cloudflare-Pages-Proxy- https://github.com/Zsfan1224/Cloudflare-Pages-Proxy</a>
